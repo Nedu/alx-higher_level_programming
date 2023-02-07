478a1fde07ae54ce57458b22cb3a2fa69b3e3e1b
Nedu Robert <nedumrobert@gmail.com>
2023-02-07 12:13:26 -0500

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list
 """
-import sys
+This module adds all arguments to a Python List
+and then save them to a file.
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+file_handle = "add_item.json"
+
+try:
+    file_content = load_from_json_file(file_handle)
+except FileNotFoundError:
+    file_content = []
+
+save_to_json_file(file_content + argv[1:], file_handle)
